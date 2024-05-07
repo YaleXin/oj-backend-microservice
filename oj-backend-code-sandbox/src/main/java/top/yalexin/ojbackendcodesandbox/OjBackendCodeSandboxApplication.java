76faa1359be263bea7e005aea0f66cc28a57753b
YaleXin <495592364@qq.com>
2024-05-07 22:23:26 +0800

    整合代码沙箱的API至网关

--- oj-backend-code-sandbox/src/main/java/top/yalexin/ojbackendcodesandbox/OjBackendCodeSandboxApplication.java
+++ oj-backend-code-sandbox/src/main/java/top/yalexin/ojbackendcodesandbox/OjBackendCodeSandboxApplication.java
@@ -3,9 +3,15 @@ package top.yalexin.ojbackendcodesandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 // 由于模块不需要和数据库打交道，因此要将数据库自动配置取消
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+// 由于全局异常类不在本模块中，因此需要手动指定扫描
+@ComponentScan("top.yalexin")
+// Nacos服务注册与发现
+@EnableDiscoveryClient
 public class OjBackendCodeSandboxApplication {
 
     public static void main(String[] args) {
