2b0c245bd2ce93b4422e9410ac6e0deb9622d148
YaleXin <495592364@qq.com>
2024-05-07 22:06:46 +0800

    代码沙箱增加返回支持的编程语言接口

--- oj-backend-code-sandbox/src/main/java/top/yalexin/ojbackendcodesandbox/controller/SandboxController.java
+++ oj-backend-code-sandbox/src/main/java/top/yalexin/ojbackendcodesandbox/controller/SandboxController.java
@@ -1,19 +1,26 @@
 package top.yalexin.ojbackendcodesandbox.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import top.yalexin.backendcommon.common.BaseResponse;
+import top.yalexin.backendcommon.common.ResultUtils;
 import top.yalexin.backendmodel.model.codesandbox.ExecuteCodeRequest;
 import top.yalexin.backendmodel.model.codesandbox.ExecuteCodeResponse;
 import top.yalexin.ojbackendcodesandbox.sandbox.DockerCodeSandboxTemplate;
+import top.yalexin.ojbackendcodesandbox.service.SandboxService;
 
 
 import javax.annotation.Resource;
+import java.util.List;
+
 @Slf4j
 @RestController("/")
 public class SandboxController {
 
     @Resource
-    private DockerCodeSandboxTemplate javaDockerCodeSandbox;
+    @Autowired
+    private SandboxService sandboxService;
 
     @PostMapping("/executeCode")
     ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest){
@@ -21,8 +28,14 @@ public class SandboxController {
             throw new RuntimeException("executeCodeRequest == null");
         }
         log.info("receive executeCodeRequest = {}", executeCodeRequest);
-        ExecuteCodeResponse executeCodeResponse = javaDockerCodeSandbox.executeCode(executeCodeRequest);
+        ExecuteCodeResponse executeCodeResponse = sandboxService.executeCode(executeCodeRequest);
         log.info("generate executeCodeResponse = {}", executeCodeResponse);
         return executeCodeResponse;
+    }
+
+    @GetMapping("/languages")
+    BaseResponse<List<String>> getLanguages(){
+        List<String> supportLanguages = sandboxService.getSupportLanguages();
+        return ResultUtils.success(supportLanguages);
     }
 }
